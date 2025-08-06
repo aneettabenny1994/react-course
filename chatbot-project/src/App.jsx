25fb7b16bec834bcd9a2e724f072b9197f37c4eb
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-08-06 13:56:09 +0000

    react exercise5: 5j save messages to localStorage

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -2,23 +2,10 @@ import { useState, useEffect } from 'react'
 import { Chatbot } from 'supersimpledev';
 import { ChatInput } from './components/ChatInput'
 import ChatMessages from './components/ChatMessages';
-import dayjs from 'dayjs';
 import './App.css'
 
 function App() {
-  useEffect(() => {
-    Chatbot.addResponses({
-      'goodbye': 'Goodbye. Have a great day!',
-      'give me a unique id': function () {
-        return `Sure! Here's a unique ID: ${crypto.randomUUID()}`;
-      }
-    });
-
-    // [] tells useEffect to only run once. We only want to run
-    // this setup code once because we only want to add these
-    // extra responses once.
-  }, []);
-  const [chatMessages, setChatMessages] = useState([
+   const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages')) || [
     {
       message: "hello Chatbot",
       sender: "user",
@@ -44,6 +31,17 @@ function App() {
       time: 1736127385500
     },
   ]);
+    useEffect(() => {
+    Chatbot.addResponses({
+      'goodbye': 'Goodbye. Have a great day!',
+      'give me a unique id': function () {
+        return `Sure! Here's a unique ID: ${crypto.randomUUID()}`;
+      }
+    });
+  }, []);
+  useEffect(() => {
+    localStorage.setItem('messages', JSON.stringify(chatMessages))
+  }, [chatMessages]);
   return (
     <>
       <div className="app-container">
