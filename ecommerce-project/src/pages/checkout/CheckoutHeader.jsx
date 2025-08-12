4a9b52a8ade6ff51f328c1a3765c387d4534cf18
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-08-12 13:07:05 +0000

    react excercise 6: 6c use Links from react-router

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -1,19 +1,20 @@
 import './CheckoutHeader.css'
+import { Link } from 'react-router'
 
 export function CheckoutHeader() {
     return (
             <div class="checkout-header">
                 <div class="header-content">
                     <div class="checkout-header-left-section">
-                    <a href="/">
+                    <Link to="/">
                         <img class="logo" src="images/logo.png" />
                         <img class="mobile-logo" src="images/mobile-logo.png" />
-                    </a>
+                    </Link>
                     </div>
 
                     <div class="checkout-header-middle-section">
-                    Checkout (<a class="return-to-home-link"
-                        href="/">3 items</a>)
+                    Checkout (<Link class="return-to-home-link"
+                        to="/">3 items</Link>)
                     </div>
 
                     <div class="checkout-header-right-section">
