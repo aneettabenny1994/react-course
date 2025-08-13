7c66cbfa15b3f4b909106201e87b0796a0d84c5c
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-08-13 06:34:06 +0000

    react exercise 6: 6k create new 404 not found page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -4,15 +4,17 @@ import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
 import { TrackingPage } from './pages/TrackingPage'
 import { Routes, Route } from 'react-router'
+import { NotFoundPage } from './pages/NotFoundPage'
 
 function App() {
   return (
-   <Routes>
-    <Route index element={<HomePage />} />
-    <Route path='checkout' element={<CheckoutPage />}></Route>
-    <Route path='orders' element={<OrdersPage />}></Route>
-    <Route path='tracking' element={<TrackingPage />}></Route>
-   </Routes>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path='checkout' element={<CheckoutPage />}></Route>
+      <Route path='orders' element={<OrdersPage />}></Route>
+      <Route path='tracking' element={<TrackingPage />}></Route>
+      <Route path='*' element={<NotFoundPage />}></Route>
+    </Routes>
   )
 }
 
