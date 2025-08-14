746962d6d5daa80adb0bc4a75e12e215882f2fe6
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-08-14 17:24:23 +0000

    extra: star rating using react

--- 25-react-projects/src/App.jsx
+++ 25-react-projects/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Accordian from './components/accordian'
 import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
 
 function App() {
 
   return (
     <>
       {/* <Accordian></Accordian> */}
-      <RandomColor></RandomColor>
+      {/* <RandomColor></RandomColor> */}
+      <StarRating noOfStars={10}></StarRating>
     </>
   )
 }
