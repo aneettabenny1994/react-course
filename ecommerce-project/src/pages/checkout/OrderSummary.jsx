db31a53101cc98512dac535af5682c4d4687f032
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-07 18:54:56 +0000

    react exercise 7: 7c separate into smaller DeliveryDate component

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,6 +1,6 @@
-import dayjs from "dayjs";
 import { DeliveryOptions } from './DeliveryOptions';
 import { CartItemDetails } from './CartItemDetails';
+import { DeliveryDate } from './DeliveryDate';
 
 export function OrderSummary({ deliveryOptions, cart }) {
     return (
@@ -11,13 +11,9 @@ export function OrderSummary({ deliveryOptions, cart }) {
                 })
                 return (
                     <div key={cartItem.productId} className="cart-item-container">
-                        <div className="delivery-date">
-                            Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-                        </div>
-
+                        <DeliveryDate selectedDeliveryOption={selectedDeliveryOption} />
                         <div className="cart-item-details-grid">
                             <CartItemDetails cartItem={cartItem} />
-
                             <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} />
                         </div>
                     </div>
