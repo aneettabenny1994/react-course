d0ab4f4a2c205d9c8b058340332044c6118b8417
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-07 19:13:55 +0000

    react exercise 7: 7g calculate totalQuantity

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -22,7 +22,7 @@ export function CheckoutPage({ cart }) {
         <>
             <title>Checkout</title>
             <link rel="icon" type="image/svg+xml" href="cart.png" />
-            <CheckoutHeader />
+            <CheckoutHeader cart={cart} />
             <div className="checkout-page">
                 <div className="page-title">Review your order</div>
                 <div className="checkout-grid">
