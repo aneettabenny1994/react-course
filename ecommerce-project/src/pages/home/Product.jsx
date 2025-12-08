4d4d88f25f532a6db89de84e17f724263c9b1b59
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-08 22:31:34 +0000

    react exercise 8: 8d 'Added' message included

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -5,12 +5,18 @@ import CheckmarkIcon from '../../assets/images/icons/checkmark.png';
 
 export function Product({ product, loadCart }) {
     const [quantity, setQuantity] = useState(1);
+    const [showAddedMessage, setShowAddedMessage] = useState(false);
     const addToCart = async () => {
         await axios.post('https://ideal-space-trout-r597pj9vrg4fwpgr-3000.app.github.dev/api/cart-items', {
             productId: product.id,
             quantity
         });
         await loadCart();
+        setShowAddedMessage(true);
+
+        setTimeout(() => {
+            setShowAddedMessage(false);
+        }, 2000);
     };
     const selectQuantity = (event) => {
         const selectedQuantityValue = Number(event.target.value);
@@ -57,7 +63,9 @@ export function Product({ product, loadCart }) {
 
             <div className="product-spacer"></div>
 
-            <div className="added-to-cart">
+            <div className="added-to-cart" style={{
+                opacity: showAddedMessage ? 1 : 0,
+            }}>
                 <img src={CheckmarkIcon} />
                 Added
             </div>
