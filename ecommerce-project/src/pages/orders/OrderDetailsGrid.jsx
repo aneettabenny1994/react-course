5cac04664cb1180368f7c98995be998db5c3efb4
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-08 18:15:45 +0000

    react exercise 8: 8a 'Add to Cart' on OrdersPage

--- ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
+++ ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
@@ -1,13 +1,24 @@
 import { Fragment } from "react";
+import axios from "axios";
 import { Link } from 'react-router'
 import dayjs from "dayjs";
 import BuyAgainIcon from '../../assets/images/icons/buy-again.png';
 
-export function OrderDetailsGrid({ order }) {
+export function OrderDetailsGrid({ order, loadCart }) {
     const orderId = order.id;
     return (
         <div className="order-details-grid">
             {order.products.map((orderProduct) => {
+                const addToCart = async () => {
+                    await axios.post('https://ideal-space-trout-r597pj9vrg4fwpgr-3000.app.github.dev/api/cart-items', {
+                        // Note: you can also get the productId from
+                        // orderProduct.productId
+                        productId: orderProduct.product.id,
+                        quantity: 1
+                    });
+                    await loadCart();
+                };
+
                 return (
                     <Fragment key={orderProduct.product.id}>
                         <div className="product-image-container">
@@ -26,7 +37,7 @@ export function OrderDetailsGrid({ order }) {
                             </div>
                             <button className="buy-again-button button-primary">
                                 <img className="buy-again-icon" src={BuyAgainIcon} />
-                                <span className="buy-again-message">Add to Cart</span>
+                                <span className="buy-again-message" onClick={addToCart}>Add to Cart</span>
                             </button>
                         </div>
 
