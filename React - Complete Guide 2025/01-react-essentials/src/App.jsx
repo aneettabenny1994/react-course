d1a887421c15c6b4ce8b43c8baef219bbad1cc9e
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-18 14:46:44 +0000

    chore: alternative syntax, spread operator usage

--- React - Complete Guide 2025/01-react-essentials/src/App.jsx
+++ React - Complete Guide 2025/01-react-essentials/src/App.jsx
@@ -1,5 +1,6 @@
 import reactImg from "./assets/react-core-concepts.png";
 import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -21,10 +22,10 @@ function Header() {
   );
 }
 
-function CoreConcept({ title, description, img }) {
+function CoreConcept({ title, description, image }) {
   return (
     <li>
-      <img src={img} alt={title} />
+      <img src={image} alt={title} />
       <h3>{title}</h3>
       <p>{description}</p>
     </li>
@@ -40,13 +41,13 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-              title="Components"
-              description="The core UI building block."
-              img={componentsImg}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <h2>Time to get started!</h2>
