320a83ab06142312509aa8a8de6f51f218038a86
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-20 09:49:17 +0000

    chore: first step towards tic-tac-toe game

--- React - Complete Guide 2025/02-tic-tac-toe-starting-project/src/App.jsx
+++ React - Complete Guide 2025/02-tic-tac-toe-starting-project/src/App.jsx
@@ -1,6 +1,21 @@
 function App() {
   return (
-    <p>Let's play Tic-Tac-Toe!</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name"> Player 1</span>
+            <span className="player-symbol"> X </span>
+          </li>
+          <li>
+            <span className="player-name"> Player 2</span>
+            <span className="player-symbol"> O </span>
+          </li>
+        </ol>
+        GAME
+      </div>
+      LOG
+    </main>
   );
 }
 
