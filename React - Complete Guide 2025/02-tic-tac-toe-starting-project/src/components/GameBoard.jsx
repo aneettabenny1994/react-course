e3ab0ac40be8f8f2a666f4d41d0deb6268bd2b7b
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-21 23:26:36 +0000

    chore: deriving computed values from other computed values

--- React - Complete Guide 2025/02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ React - Complete Guide 2025/02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,32 +1,7 @@
-import { useState } from "react";
-
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
-
-export function GameBoard({ onSelectSquare, turns }) {
-    let gameBoard = initialGameBoard;
-    for (const turn of turns) {
-        const { row, col } = turn.square;
-        gameBoard[row][col] = turn.player;
-    }
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // function handleSelectSquare(rowIndex, colIndex) {
-    //   setGameBoard((previousGameBoard) => {
-    //     const updatedGameBoard = [
-    //       ...previousGameBoard.map((innerArray) => [...innerArray]),
-    //     ];
-    //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //     return updatedGameBoard;
-    //   });
-    //   onSelectSquare();
-    // }
+export function GameBoard({ onSelectSquare, board }) {
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex} className="board-row">
                     <ol className="board-row-list">
                         {row.map((playerSymbol, colIndex) => (
