5770ddac5284a48f5562e9194754ca0f3ca50cc8
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-23 21:45:34 +0000

    chore: creating reusable components & Component combinations

--- React - Complete Guide 2025/04-styling-react-components/src/components/AuthInputs.jsx
+++ React - Complete Guide 2025/04-styling-react-components/src/components/AuthInputs.jsx
@@ -1,46 +1,13 @@
 import { useState } from "react";
 import { styled } from "styled-components";
+import Button from "./Button";
+import Input from "./Input";
 
 const ControlContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`;
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#374151")};
-  border: 1px solid ${({ $invalid }) => ($invalid ? "#f87171" : "transparent")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-  &:hover {
-    background-color: #f0920e;
-  }
 `;
 
 export default function AuthInputs() {
@@ -67,8 +34,8 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer className="controls">
         <p>
-          <Label $invalid={emailNotValid}>Email</Label>
           <Input
+            label="Email"
             type="email"
             // style={{ backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db" }}
             $invalid={emailNotValid}
@@ -76,8 +43,8 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
+            label="Password"
             type="password"
             $invalid={passwordNotValid}
             onChange={(event) =>
