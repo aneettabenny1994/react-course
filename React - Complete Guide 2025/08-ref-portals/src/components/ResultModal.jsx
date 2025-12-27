44f48d57a105461bb63725f27e26a1387f6caf3e
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-27 21:14:41 +0000

    chore: useImperativeHandle hook of React

--- React - Complete Guide 2025/08-ref-portals/src/components/ResultModal.jsx
+++ React - Complete Guide 2025/08-ref-portals/src/components/ResultModal.jsx
@@ -1,12 +1,21 @@
-
+import { useRef } from "react";
+import { useImperativeHandle } from "react";
 import { forwardRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
     { result, targetTime },
     ref
 ) {
+    const dialog = useRef();
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            },
+        };
+    });
     return (
-        <dialog ref={ref} className="result-modal">
+        <dialog ref={dialog} className="result-modal">
             <h2>You {result}</h2>
             <p>
                 The target time was {targetTime} second{targetTime !== 1 ? "s" : ""}.
