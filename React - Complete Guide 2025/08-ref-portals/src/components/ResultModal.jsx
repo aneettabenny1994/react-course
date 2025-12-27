2dc06bb0815438aa5092dba5546704d60f4c56cb
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-27 21:02:17 +0000

    chore: forwardRef for versions less than React 19 (still can be used)

--- React - Complete Guide 2025/08-ref-portals/src/components/ResultModal.jsx
+++ React - Complete Guide 2025/08-ref-portals/src/components/ResultModal.jsx
@@ -1,4 +1,10 @@
-export default function ResultModal({ ref, result, targetTime }) {
+
+import { forwardRef } from "react";
+
+const ResultModal = forwardRef(function ResultModal(
+    { result, targetTime },
+    ref
+) {
     return (
         <dialog ref={ref} className="result-modal">
             <h2>You {result}</h2>
@@ -13,4 +19,6 @@ export default function ResultModal({ ref, result, targetTime }) {
             </form>
         </dialog>
     );
-}
+});
+
+export default ResultModal;
