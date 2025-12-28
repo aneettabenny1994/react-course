9d96001e023abe71f8289d7f2c7fe4e8e2ce9576
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-28 13:26:14 +0000

    chore: adding the 'new project' component & A reusable 'Input' component

--- React - Complete Guide 2025/09-project-adder/src/App.jsx
+++ React - Complete Guide 2025/09-project-adder/src/App.jsx
@@ -1,10 +1,12 @@
+import NewProject from "./components/NewProject";
 import ProtectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
     <>
-      <main className="h-screen my-8">
+      <main className="h-screen my-8 flex gap-8">
         <ProtectsSidebar />
+        <NewProject />
       </main>
     </>
   );
