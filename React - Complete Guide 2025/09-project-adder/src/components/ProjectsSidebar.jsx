5a1f873dc9e5111d921bbc85f4942d73077e2650
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-28 13:07:43 +0000

    chore: adding a projects sidebar component

---
+++ React - Complete Guide 2025/09-project-adder/src/components/ProjectsSidebar.jsx
@@ -0,0 +1,10 @@
+export default function ProtectsSidebar() {
+    return (
+        <aside>
+            <h2>Your projects</h2>
+            <div>
+                <button>+ Add Project</button>
+            </div>
+        </aside>
+    );
+}
