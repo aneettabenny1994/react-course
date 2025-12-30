901820899cff1f1399e00229961c6f464e19bde6
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2025-12-30 17:46:41 +0000

    chore: handling Project deletion

--- React - Complete Guide 2025/09-project-adder/src/App.jsx
+++ React - Complete Guide 2025/09-project-adder/src/App.jsx
@@ -43,10 +43,25 @@ function App() {
       };
     });
   }
+
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      const updatedProjects = prevState.projects.filter(
+        (project) => project.id !== prevState.selectedProjectId
+      );
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: updatedProjects,
+      };
+    });
+  }
   const selectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId
   );
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
 
   if (projectsState.selectedProjectId === null) {
     content = (
