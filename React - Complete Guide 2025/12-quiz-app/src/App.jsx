9e751e517c5ad3be14c460c7538df103fbdaa382
Aneetta Benny <104563124+aneettabenny1994@users.noreply.github.com>
2026-01-04 18:24:54 +0000

    chore: a first component & some state

--- React - Complete Guide 2025/12-quiz-app/src/App.jsx
+++ React - Complete Guide 2025/12-quiz-app/src/App.jsx
@@ -1,3 +1,11 @@
-function App() {}
+import Header from "./components/Header.jsx";
+
+function App() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
 
 export default App;
---
+++ React - Complete Guide 2025/12-quiz-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logImg from "../assets/quiz-logo.png";
+export default function Header() {
+  return (
+    <header>
+      <img src={logImg} alt="Quiz Logo" />
+      <h1>ReactQuiz</h1>
+    </header>
+  );
+}
